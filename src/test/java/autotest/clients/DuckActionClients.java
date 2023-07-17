890f5dc91f45c736fd8afc298d25e72f11901d15
kovalenkoam <kowalalex@mail.ru>
2023-07-17 12:34:32 +0700

    Выполнение ДЗ к лекции "Структура проекта автотестов. Clients" Добавление: - класса EndpointConfig.java - класса DuckActionClients.java

---
+++ src/test/java/autotest/clients/DuckActionClients.java
@@ -0,0 +1,114 @@
+package autotest.clients;
+
+import autotest.EndpointConfig;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import com.consol.citrus.http.client.HttpClient;
+
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+@ContextConfiguration(classes = {EndpointConfig.class})
+public class DuckActionClients extends TestNGCitrusSpringSupport {
+    @Autowired
+    protected HttpClient yellowDuckService;
+
+    public void duckFly(TestCaseRunner runner, String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/fly")
+                .queryParam("id", id));
+    }
+
+    public void duckSwim(TestCaseRunner runner, String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/swim")
+                .queryParam("id", id));
+    }
+
+    public void duckQuack(TestCaseRunner runner,
+                          String id,
+                          String repetitionCount,
+                          String soundCount) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/swim")
+                .queryParam("id", id)
+                .queryParam("repetitionCount", repetitionCount)
+                .queryParam("soundCount", soundCount));
+    }
+
+    public void duckShowProperties(TestCaseRunner runner,
+                                   String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/action/swim")
+                .queryParam("id", id));
+    }
+
+    public void duckCreate(TestCaseRunner runner,
+                           String color,
+                           String height,
+                           String material,
+                           String sound,
+                           String wingsState) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" +
+                        "  \"color\": \"" + color + "\",\n" +
+                        "  \"height\": " + height + ",\n" +
+                        "  \"material\": \"" + material + "\",\n" +
+                        "  \"sound\": \"" + sound + "\",\n" +
+                        "  \"wingsState\": \"" + wingsState + "\"\n" +
+                        "}"));
+    }
+
+    public void duckDelete(TestCaseRunner runner,
+                           String id) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .delete("/api/duck/delete")
+                .queryParam("id", id));
+    }
+
+    public void duckGetAllIds(TestCaseRunner runner) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .get("/api/duck/getAllIds"));
+    }
+
+    public void duckUpdate(TestCaseRunner runner,
+                           String id,
+                           String color,
+                           String height,
+                           String material,
+                           String sound,
+                           String wingsState) {
+        runner.$(http()
+                .client(yellowDuckService)
+                .send()
+                .put("/api/duck/update")
+                .message()
+                .queryParam("id", id)
+                .queryParam("color", color)
+                .queryParam("height", height)
+                .queryParam("material", material)
+                .queryParam("sound", sound)
+                .queryParam("wingsState", wingsState));
+    }
+}
+
+
